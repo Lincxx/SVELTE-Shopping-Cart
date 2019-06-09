f3833554c11911b93db1af5be14dbb38c2443097
Lincxx <bossco28@gmail.com>
2019-06-09 13:11:11 -0400

    Added Cart Comp, Prod Comp, Button Comp

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,85 @@
 <script>
-	export let name;
+  import Product from "./Product.svelte";
+  import Button from "./Button.svelte";
+  import Cart from "./Cart.svelte";
+
+  let title = "";
+  let price = 0;
+  let description = "";
+
+  let products = [];
+  let cartItems = [];
+
+  function setTitle(event) {
+    title = event.target.value;
+  }
+
+  function createProduct() {
+    const newProduct = {
+      title: title,
+      price: price,
+      description: description
+    };
+
+    products = products.concat(newProduct);
+  }
+  function addToCart(event) {
+    const selectedTitle = event.detail;
+    cartItems = cartItems.concat({
+      ...products.find(prod => prod.title === selectedTitle)
+    });
+    console.log(cartItems);
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  section {
+    width: 30rem;
+    margin: auto;
+  }
+
+  label,
+  input,
+  textarea {
+    width: 100%;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<section>
+  <Cart items={cartItems} />
+</section>
+
+<hr />
+
+<section>
+  <div>
+    <label for="title">Title</label>
+    <input type="text" id="title" value={title} on:input={setTitle} />
+  </div>
+
+  <div>
+    <label for="price">Price</label>
+    <input type="number" id="price" bind:value={price} />
+  </div>
+
+  <div>
+    <label for="description">Description</label>
+    <textarea rows="3" id="description" bind:value={description} />
+  </div>
+
+  <Button on:click={createProduct}>Create Product</Button>
+</section>
+
+<section>
+  {#if products.length === 0}
+    <p>No porducts were added yet!</p>
+  {:else}
+    {#each products as product}
+      <Product
+        productTitle={product.title}
+        productDescription={product.description}
+        productPrice={product.price}
+        on:addcart={addToCart} />
+    {/each}
+  {/if}
+</section>
